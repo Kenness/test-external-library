c758cd0b993d3b890e2f8bd3232567640afb27a9
Ha Le <ha.lequang1990@gmail.com>
2018-12-21 15:36:37 +0700

    First Commit

---
+++ src/main/java/jayeson/lib/Common.java
@@ -0,0 +1,15 @@
+package jayeson.lib;
+
+public class Common {
+	public static void printSomething() {
+		System.out.println("Hello World!!!");
+	}
+
+	public static String getVersion() {
+		return "Version 1.0";
+	}
+
+	public static String getString() {
+		return "Hello World";
+	}
+}
